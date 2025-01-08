b0f8b4714c1281f300b7a96bbc3262a2c056c686
dipsy-hash <dipsy@xray.app>
2025-01-08 20:41:46 +0300

    Shcema generation from URL (Hono generated)

--- src/schema/generate.mjs
+++ src/schema/generate.mjs
@@ -1,11 +1,16 @@
 import fs from "fs"
-import openapiTS from "openapi-typescript"
+import openapiTS, { astToString } from "openapi-typescript"
 
 const run = async () => {
-  const schema = await openapiTS("./src/schema/schema.yaml", {
-    exportType: true,
-  })
-  fs.writeFileSync("./src/schema/schema.ts", schema)
+  const schema = (await openapiTS(
+    new URL(
+      "https://graph.xray.app/output/nftcdn/mainnet/api/v1/openapi.json"
+    ),
+    {
+      exportType: true,
+    }
+  ))
+  fs.writeFileSync("./src/schema/schema.ts", astToString(schema))
 }
 
 run()
